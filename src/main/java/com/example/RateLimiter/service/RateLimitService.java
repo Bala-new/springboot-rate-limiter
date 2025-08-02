f143fc7575a32443291fd3b99dc3c34079ca6a76
bala <balamuruganantham74@gmail.com>
2025-08-02 12:59:59 +0530

    rate limiter using sliding window without db

--- src/main/java/com/example/RateLimiter/service/RateLimitService.java
+++ src/main/java/com/example/RateLimiter/service/RateLimitService.java
@@ -1,31 +1,33 @@
 package com.example.RateLimiter.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.*;
+
+
 import org.springframework.stereotype.Service;
-import com.example.RateLimiter.model.RequestInfo;
-import com.example.RateLimiter.repository.RateLimitRepository;
 
 @Service
 public class RateLimitService {
 
+    private Map<String, Deque<Long>> requestinfoMap= new HashMap<>();
     private static final long WINDOW_SIZE_MS = 60 * 1000; // 1 minute
     private static final int MAX_REQUESTS = 5;
 
-    @Autowired
-    private RateLimitRepository ratelimitRepository;
-
+  
     public boolean isAllowed(String userIp) {
         long currentTime = System.currentTimeMillis();
-        RequestInfo info = ratelimitRepository.findById(userIp).orElse(new RequestInfo(userIp, 0, currentTime));
+        Deque<Long> timeStamps=requestinfoMap.getOrDefault(userIp, new ArrayDeque<>());
 
-        if (currentTime - info.getWindowStartTimestamp() > WINDOW_SIZE_MS) {
-            info.setWindowStartTimestamp(currentTime);
-            info.setRequestCount(1);
-        } else {
-            info.setRequestCount(info.getRequestCount() + 1);
+        while(!timeStamps.isEmpty() && currentTime - timeStamps.peekFirst() > WINDOW_SIZE_MS) {
+            timeStamps.pollFirst();
         }
+        
 
-        ratelimitRepository.save(info);
-        return info.getRequestCount() <= MAX_REQUESTS;
+        if (timeStamps.size() < MAX_REQUESTS) {
+            timeStamps.addLast(currentTime);
+            requestinfoMap.put(userIp,timeStamps);
+            return true;
+        } 
+
+        return false;
     }
 }
